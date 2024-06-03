581075f83d1ef9b214029fc4fa8155f2e943aaab
prof-hissa <141635865+prof-hissa@users.noreply.github.com>
2024-06-03 09:38:47 -0300

    Adição de comentários explicativos ao projeto - ContaPoupanca.java

--- banco-simples/src/ContaPoupanca.java
+++ banco-simples/src/ContaPoupanca.java
@@ -1,4 +1,8 @@
-
+/*
+  Classe filha da classe Conta; Note que essa classe apenas implementa o método imprimirExtrato e um construtor.
+  Todo o resto é herdado da classe pai (Conta). Além disso, nesta classe existe uma sobrescrita do método sacar 
+  apenas com intuito didádico de demonstração.
+*/
 public class ContaPoupanca extends Conta {
 
     private static final double LIMITE_SAQUE = 500;
@@ -20,6 +24,8 @@ public class ContaPoupanca extends Conta {
 
     public void imprimirExtrato() {
         System.out.println("=== Extrato Conta Poupança ===");
+
+        // O uso da palavra super, indica que o método imprimirInfosComuns está sendo invocado da super classe (Conta)
         super.imprimirInfosComuns();
     }
 }

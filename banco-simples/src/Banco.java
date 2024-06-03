eac5fb87adc61f5999e908c13fcf7a1c966cc883
prof-hissa <141635865+prof-hissa@users.noreply.github.com>
2024-06-03 08:55:47 -0300

    Adição de comentários explicativos ao projeto - Banco.java

--- banco-simples/src/Banco.java
+++ banco-simples/src/Banco.java
@@ -1,6 +1,13 @@
 
 import java.util.List;
 
+/*
+  Está é somente uma classe que armazena o nome do banco e 
+  uma lista de contas que esse banco possui. Em um sistema 
+  real, essa classe precisaria de mais implementações como
+  getters e setter, construtores, etc. Fique a vontade para
+  adicionar essas implementações.
+*/
 public class Banco {
 
     private String nome;
